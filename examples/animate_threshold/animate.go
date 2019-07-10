274efedfe294fdf245efc1d6036af15783ba088c
Kerby Shedden <kshedden@umich.edu>
2019-07-10 11:03:51 -0400

    update example

--- examples/animate_threshold/animate.go
+++ examples/animate_threshold/animate.go
@@ -8,11 +8,28 @@ import (
 
 const (
 	fn = "HeLa_cells_stained_with_antibody_to_actin_(green)_,_vimentin_(red)_and_DNA_(blue).jpg"
+
+	xoff = 280
+	yoff = 250
+
+	xwid = 350
+	ywid = 350
 )
 
 func main() {
 
 	img, rows := tda.GetImage(path.Join("../images", fn))
+	cols := len(img) / rows
 
-	tda.AnimateThreshold(img, rows, 100, 80000, 200, "../images/cells.apng")
+	// Crop the image
+	img2 := make([]int, xwid*ywid)
+	ii := 0
+	for j := yoff; j < yoff+ywid; j++ {
+		for i := xoff; i < xoff+xwid; i++ {
+			img2[ii] = img[i*cols+j]
+			ii++
+		}
+	}
+
+	tda.AnimateThreshold(img2, ywid, 100, 80000, 200, "../images/cells.apng")
 }
